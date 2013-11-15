33d7ce79e9cc338c7735c62bac36481a0305bb5e
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-11-15 10:59:54 +0100

    #5 Create a memmory as a Map for builds

---
+++ docroot/WEB-INF/src/com/liferay/ci/jenkins/cache/LiferayJenkinsBuildCache.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ci.jenkins.cache;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.json.JSONArray;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class LiferayJenkinsBuildCache {
+
+	public LiferayJenkinsBuildCache() {
+		_initialize();
+	}
+
+	public void clear() {
+		for (String key : _buildsCache.keySet()) {
+			clear(key);
+		}
+	}
+
+	public void clear(String portletId) {
+		Map<String, JSONArray> portletCache = _buildsCache.get(portletId);
+
+		portletCache.clear();
+	}
+
+	public boolean containsKey(String portletId, Object key) {
+		Map<String, JSONArray> portletCache = _buildsCache.get(portletId);
+
+		return portletCache.containsKey(key);
+	}
+
+	public boolean containsValue(String portletId, Object value) {
+		Map<String, JSONArray> portletCache = _buildsCache.get(portletId);
+
+		return portletCache.containsValue(value);
+	}
+
+	public Set<java.util.Map.Entry<String, JSONArray>> entrySet(
+			String portletId) {
+
+		Map<String, JSONArray> portletCache = _buildsCache.get(portletId);
+
+		return portletCache.entrySet();
+	}
+
+	public JSONArray get(String portletId, Object key) {
+		Map<String, JSONArray> portletCache = _buildsCache.get(portletId);
+
+		return portletCache.get(key);
+	}
+
+	public boolean isEmpty(String portletId) {
+		Map<String, JSONArray> portletCache = _buildsCache.get(portletId);
+
+		return portletCache.isEmpty();
+	}
+
+	public Set<String> keySet(String portletId) {
+		Map<String, JSONArray> portletCache = _buildsCache.get(portletId);
+
+		return portletCache.keySet();
+	}
+
+	public JSONArray put(String portletId, String key, JSONArray value) {
+		Map<String, JSONArray> portletCache = _buildsCache.get(portletId);
+
+		return portletCache.put(key, value);
+	}
+
+	public void putAll(
+		String portletId, Map<? extends String, ? extends JSONArray> m) {
+
+		Map<String, JSONArray> portletCache = _buildsCache.get(portletId);
+
+		portletCache.putAll(m);
+	}
+
+	public JSONArray remove(String portletId, Object key) {
+		Map<String, JSONArray> portletCache = _buildsCache.get(portletId);
+
+		return portletCache.remove(key);
+	}
+
+	public int size(String portletId) {
+		Map<String, JSONArray> portletCache = _buildsCache.get(portletId);
+
+		return portletCache.size();
+	}
+
+	public Collection<JSONArray> values(String portletId) {
+		Map<String, JSONArray> portletCache = _buildsCache.get(portletId);
+
+		return portletCache.values();
+	}
+
+	private void _initialize() {
+		_buildsCache = new HashMap<String, Map<String, JSONArray>>();
+	}
+
+	private Map<String, Map<String, JSONArray>> _buildsCache;
+
+}
